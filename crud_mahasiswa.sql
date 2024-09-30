1b97de260c01441a3018d006b5f44a00363dcb50
Admsrzky <adimasrizki926@gmail.com>
2024-09-30 11:51:52 +0700

    update library

--- crud_mahasiswa.sql
+++ crud_mahasiswa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2024 at 09:42 AM
+-- Generation Time: Sep 30, 2024 at 06:50 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,42 +24,68 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mahasiswa`
+-- Table structure for table `mahasiswas`
 --
 
-CREATE TABLE `mahasiswa` (
+CREATE TABLE `mahasiswas` (
   `id` int(11) NOT NULL,
-  `nama` varchar(100) NOT NULL,
-  `jurusan` varchar(100) NOT NULL
+  `nama` varchar(255) DEFAULT NULL,
+  `jurusan` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `mahasiswa`
+-- Dumping data for table `mahasiswas`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nama`, `jurusan`) VALUES
-(1, 'Adimas Rizki Purwanto', 'Sistem Informasi'),
-(2, 'Ghefira salsabila Calistra', 'Teknik Informatika');
+INSERT INTO `mahasiswas` (`id`, `nama`, `jurusan`, `createdAt`, `updatedAt`) VALUES
+(1, 'Adimas Rizki Purwanto', 'Sistem Informasi', '2024-09-30 04:14:21', '2024-09-30 04:14:56'),
+(2, 'Ghefira salsabila Calistra', 'Teknik Informasi', '2024-09-30 04:15:13', '2024-09-30 04:21:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sequelizemeta`
+--
+
+CREATE TABLE `sequelizemeta` (
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `sequelizemeta`
+--
+
+INSERT INTO `sequelizemeta` (`name`) VALUES
+('20240930035807-create-mahasiswa.js');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `mahasiswa`
+-- Indexes for table `mahasiswas`
 --
-ALTER TABLE `mahasiswa`
+ALTER TABLE `mahasiswas`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sequelizemeta`
+--
+ALTER TABLE `sequelizemeta`
+  ADD PRIMARY KEY (`name`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `mahasiswa`
+-- AUTO_INCREMENT for table `mahasiswas`
 --
-ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `mahasiswas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
